7479d8b4d857dc5881859dd11e1092f6d7c2f6b9
Yashwant Keswani <yashwant.keswani@gmail.com>
2016-04-07 18:51:30 +0530

    Inverse Transform Added. Bug Fixes

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
 from model_creator import token_generator
-from copy import deepcopy
+import random
+from operator import itemgetter
+
 class model:
 	t=token_generator()
-	
+	random.seed()	
 	def generate_model(self,order,filename):
 		tokens=self.t.generate(filename)
 		states={'{{start}}':{'total':0}}
 
 		for i in tokens:
+
+			if len(i)<order:
+				continue
 
 			init_tuple=tuple([i[x] for x in xrange(1,order)])
 			print init_tuple
@@ -33,9 +38,25 @@ class model:
 					states[temp_tuple]={'total':0}
 					states[temp_tuple][temp_tuple_2]=1
 					states[temp_tuple]['total']+=1
+			end_tuple = tuple([i[x] for x in xrange(j-order,len(i))])
+			try:
+				states[end_tuple]['{{end}}'] += 1;
+				states[end_tuple]['total'] += 1;
+			except:
+				states[end_tuple]['{{end}}'] = 1;
+				states[end_tuple]['total'] += 1;
+						
 		return states
 
-	
+	def inverseTransform(probable_states):
+		U = random.rand();
+		outlinks = probable_states['total']
+		sorted_probable_states = sorted(probable_states.items(), key=itemgetter(1))
+		for i in sorted_probable_states:
+			if U<float(sorted_probable_states[i])/float(total):
+				return i 
+			
+			
 
 if __name__=='__main__':
 	m=model()
